599fcf351a25689c1295c1e3109556f24a726623
Vincent <defspyvc41@yandex.com>
2021-02-27 17:53:06 -0500

    Clarify code & make button processing efficient
    Instead of running the same function on several parameters (i.e. checking each
    digit 1-9 separately and then checking each operand), this patch will
    efficiently check the type of event and then respond to it by updating the
    appropriate operands or operator list.
    
    Some helper functions are removed as a result.
    
    In addition, the 'ANS' function has been implemented: the accumulator (result of
    an arithmetic expression) is displayed in its own text box as part of the UI.
    
    Finally, '0' has been added as a valid operand, which hadn't been supported before.

--- calculator.py
+++ calculator.py
@@ -1,85 +1,107 @@
-import PySimpleGUI as sg
-#calculations
+#!/usr/bin/env python3
 
-current = ''
-valuecurrent = 0
-Beforefinished=0
-Afterfinished=0
+# == step 0: import dependencies ==
+import PySimpleGUI as sg
+
+# == step 1: set variables ==
+# TODO better encapsulate these fields into a Calculator object
+currentToken = ''
+currentTokenAsInt = 0
+Beforefinished = 0
+Afterfinished = 0
 
 
 numberList = []
 operatorList = []
 
-def calculation(numberList, operatorList):
-    storeNumber = int(numberList[0])
-    for i in range(0, len(numberList)-1):
-        if operatorList[i] == "+":
-            storeNumber = storeNumber + int(numberList[i+1])
-        elif operatorList[i] == "-":
-            storeNumber = storeNumber - int(numberList[i+1])
-        elif operatorList[i] == "*":
-            storeNumber = storeNumber * int(numberList[i+1])
-        elif operatorList[i] == "/":
-            storeNumber = storeNumber / int(numberList[i+1])
-    return storeNumber
-
-def numbers(value):
-    if event in (None, value):
-        global valuecurrent
-        global current
-        #makes it into a string
-        valuecurrent=value
-        current = current + valuecurrent
-        valuecurrent = current
-        print(current)
-
-def symbols(types):
-    if event in (None, types):
-        global current
-        global valuecurrent
-        numberList.append(current)
-        operatorList.append(types)
-        current=''
-        valuecurrent=''
-        print(numberList)
-        print(operatorList)
-        
-def unspaghettify_this():    
-    numbers('1')
-    numbers('2')
-    numbers('3')
-    numbers('4')
-    numbers('5')
-    numbers('6')
-    numbers('7')
-    numbers('8')
-    numbers('9')
-    symbols('+')
-    symbols('-')
-    symbols('*')
-    symbols('/')
-
-#PSG part
+# == step 2: PSG setup ==
+# Under the current design, this needs to happen sooner in order for the actual
+# calculator business logic to update the "Answer" indicator
 
 sg.change_look_and_feel('DarkAmber')	# Add a touch of color
 # All the stuff inside your window.
 layout = [  [sg.Text('God has left us, and programming is why - the calculator version')],
-            [sg.Text()],
-            [sg.Button('1'), sg.Button('2'), sg.Button('3'), sg.Button('+')], 
-            [sg.Button('4'), sg.Button('5'), sg.Button('6'), sg.Button('-')], 
-            [sg.Button('7'), sg.Button('8'), sg.Button('9'), sg.Button('/')], 
+            [sg.Text(size=(64, 1), key="ACC")],
+            [sg.Button('1'), sg.Button('2'), sg.Button('3'), sg.Button('+')],
+            [sg.Button('4'), sg.Button('5'), sg.Button('6'), sg.Button('-')],
+            [sg.Button('7'), sg.Button('8'), sg.Button('9'), sg.Button('/')],
+            [sg.Button('0')],
             [sg.Button('ANS'), sg.Button('CE'), sg.Button('x'), sg.Button('*')],
-            [sg.Button('GOD MAKE IT STOP PLEASE STOP IT')],]
+            [sg.Button('GOD MAKE IT STOP PLEASE STOP IT')]]
 
 # Create the Window
 window = sg.Window('Window Title', layout)
+
+# == step 3: calculator business logic ==
+def evaluateFullExpression():
+    accumulator = numberList[0]
+    for i in range(0, len(numberList)-1):
+        # TODO make an enum type for the OPERATOR
+        # it's times like these when
+        # I wish python 3 had a built-in switch/case feature...
+        if operatorList[i] == "+":
+            accumulator = accumulator + numberList[i+1]
+        elif operatorList[i] == "-":
+            accumulator = accumulator - numberList[i+1]
+        elif operatorList[i] == "*":
+            accumulator = accumulator * numberList[i+1]
+        elif operatorList[i] == "/":
+            if (numberList[i+1] == 0):
+                print("ERR: Division by zero detected!")
+                exit()
+            else:
+                accumulator = accumulator / numberList[i+1]
+        else:
+            print("ERR: button identifier %s not supported" % operatorList[i])
+    return accumulator
+
+# A function that responds to each button click (determined by its unique
+#     identifier) by updating the relevant lists.
+def process_next_button(identifier):
+    # I still hate this "globals" practice and think we should refactor it
+    #     into an object or something
+    global currentTokenAsInt
+    global currentToken
+    global numberList
+    global operatorList
+    if (identifier == None or len(identifier) < 1):
+        print("ERR: button identifier is read as None or empty string. Check button definitions.")
+    elif (identifier[0] >= '0' and identifier[0] <= '9'): 
+        # Note: the above checks are deliberate. We want to use ASCII values
+        # (namely 48 and 57 in decimal) for the CHARACTERS '0' through '9'
+        # notice, this ensures that currentTokenAsInt is a String type
+        currentToken = currentToken + identifier
+        currentTokenAsInt = int(currentToken)
+        print(currentToken)
+    elif (identifier == "ANS"):
+        numberList.append(currentTokenAsInt)
+        accumulator = evaluateFullExpression()
+        print(accumulator)
+
+        # here, we want to clear all variables to their defaults
+        numberList = []
+        operatorList = []
+        currentToken = ""
+        currentTokenAsInt = 0
+        # update the GUI with the accumulator somehow
+        window['ACC'].update('ANS: '+str(accumulator))
+    else:
+        # we delay evaluation until 'ANS' button is clicked
+        numberList.append(currentTokenAsInt)
+        operatorList.append(identifier)
+        currentToken = ""
+        currentTokenAsInt = 0
+#        print(numberList)
+#        print(operatorList)
+
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
     if event in (None, 'GOD MAKE IT STOP PLEASE STOP IT'):	# if user closes window or clicks cancel
         break
-    unspaghettify_this()
+    # extract the actual identifier for the button pressed;
+    # pass it in as parameter.
+    process_next_button(event)
 
 window.close()
-
 
